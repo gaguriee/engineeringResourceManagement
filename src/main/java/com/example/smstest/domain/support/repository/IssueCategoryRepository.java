f6a27a864f58d55b76d6a7e272f4a9f35cea07e9
gaguriee <newkgg112@gmail.com>
2023-10-25 17:45:27 +0900

    MODIFY : Dynamic Height in Timeline of WBS

--- src/main/java/com/example/smstest/domain/support/repository/IssueCategoryRepository.java
+++ src/main/java/com/example/smstest/domain/support/repository/IssueCategoryRepository.java
@@ -3,13 +3,14 @@ package com.example.smstest.domain.support.repository;
 import com.example.smstest.domain.support.entity.IssueCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface IssueCategoryRepository extends JpaRepository<IssueCategory, Long> {
 
     @Query("SELECT ic FROM IssueCategory ic WHERE ic.visibility = true AND (ic.divisionId is null OR ic.divisionId = :divisionId) ORDER BY ic.priority")
-    List<IssueCategory> findAllOrderedByPriority(Integer divisionId);
+    List<IssueCategory> findAllOrderedByPriority(@Param("divisionId") Integer divisionId);
 
 }
 
