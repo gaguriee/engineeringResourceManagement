fd29fe2507d6e1310b61878d20571a02337cdcd7
gaguriee <newkgg112@gmail.com>
2024-02-06 14:12:14 +0900

    마무리 240226

--- src/main/java/com/example/smstest/domain/project/ProjectController.java
+++ src/main/java/com/example/smstest/domain/project/ProjectController.java
@@ -7,6 +7,11 @@ import com.example.smstest.domain.client.Client;
 import com.example.smstest.domain.client.ClientRepository;
 import com.example.smstest.domain.organization.entity.Team;
 import com.example.smstest.domain.organization.repository.TeamRepository;
+import com.example.smstest.domain.project.dto.ProjectRequest;
+import com.example.smstest.domain.project.entity.DeliveryEquipment;
+import com.example.smstest.domain.project.entity.Project;
+import com.example.smstest.domain.project.repository.DeliveryEquipmentRepository;
+import com.example.smstest.domain.project.repository.ProjectRepository;
 import com.example.smstest.domain.support.entity.Product;
 import com.example.smstest.domain.support.entity.Support;
 import com.example.smstest.domain.support.repository.ProductRepository;
@@ -31,10 +36,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * WBS (Project 별 지원 내역 및 일정 조회) 페이지 매핑 Controller
@@ -53,6 +60,7 @@ public class ProjectController {
     private final TaskCategoryRepository taskCategoryRepository;
     private final ClientRepository clientRepository;
     private final ProductRepository productRepository;
+    private final DeliveryEquipmentRepository deliveryEquipmentRepository;
     private final TeamRepository teamRepository;
 
     @InitBinder
@@ -64,6 +72,7 @@ public class ProjectController {
     /**
      * [ 프로젝트 현황 ]
      * 프로젝트 리스트 가져오기
+     *
      * @param Keyword
      * @param pageable
      * @param model
@@ -88,6 +97,7 @@ public class ProjectController {
     /**
      * [ 지원내역 등록 내 ]
      * 라이센스 프로젝트 리스트 가져오기
+     *
      * @param Keyword
      * @param pageable
      * @param model
@@ -116,14 +126,15 @@ public class ProjectController {
      * - 프로젝트 정보
      * - 해당 프로젝트 관련 일정 리스트
      * - 해당 프로젝트 관련 지원내역 Pageable 객체
+     *
      * @param projectId
      * @param pageable
      * @param model
      * @return
      */
-    @GetMapping("/detail")
+    @GetMapping("/{projectId}")
     public String viewProjectDetail(
-            @RequestParam(required = false) Long projectId,
+            @PathVariable Long projectId,
             @PageableDefault(size = 15) Pageable pageable,
             Model model) {
 
@@ -148,12 +159,13 @@ public class ProjectController {
 
         // 프로젝트 수정을 위한 DTO 객체 생성
         ProjectRequest projectRequest = ProjectRequest.builder()
+                .projectId(projectId)
                 .engineerId(Optional.ofNullable(project.getEngineer()).map(Memp::getId).orElse(null))
                 .subEngineerId(Optional.ofNullable(project.getSubEngineer()).map(Memp::getId).orElse(null))
                 .teamId(Optional.ofNullable(project.getTeam()).map(Team::getId).orElse(null))
                 .productId(Optional.ofNullable(project.getProduct()).map(Product::getId).orElse(null))
                 .orderAmount(project.getOrderAmount())
-                .equipment(project.getEquipment())
+                .deliveryEquipmentList(deliveryEquipmentRepository.findAllByProjectId(project.getId()))
                 .build();
 
         projectRequest.setProjectId(project.getId());
@@ -174,13 +186,14 @@ public class ProjectController {
 
     /**
      * [ 프로젝트 수정 ]
+     *
      * @param projectRequest
      * @param model
      * @return
      */
     @PostMapping("/modify")
-    public String modifyProject(
-            ProjectRequest projectRequest, Model model) {
+    @Transactional
+    public String modifyProject(ProjectRequest projectRequest, Model model) {
 
         Memp user = mempRepository.findFirstByUsernameAndActiveTrue(SecurityContextHolder.getContext().getAuthentication().getName())
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
@@ -197,11 +210,23 @@ public class ProjectController {
                         .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND)),
                 projectRequest.getSubEngineerId() == null ? null :
                         mempRepository.findById(projectRequest.getSubEngineerId()).orElse(null),
-                projectRequest.getOrderAmount(),
-                projectRequest.getEquipment()
+                projectRequest.getOrderAmount()
         );
 
+        newProject.setDeliveryEquipmentList(null);
         projectRepository.save(newProject);
+
+        deliveryEquipmentRepository.deleteAllByProject(newProject);
+
+        List<DeliveryEquipment> deliveryEquipmentRequests = projectRequest.getDeliveryEquipmentList().stream()
+                .map(DeliveryEquipment::of)
+                .filter(equipment -> equipment.getEquipmentName() != null && equipment.getQuantity() != null)
+                .peek(equipment -> {
+                    equipment.setProject(newProject);
+                })
+                .collect(Collectors.toList());
+
+        deliveryEquipmentRepository.saveAll(deliveryEquipmentRequests);
 
         model.addAttribute("project", newProject);
 
@@ -223,7 +248,7 @@ public class ProjectController {
         model.addAttribute("products", products);
         model.addAttribute("teams", teams);
 
-        return "redirect:/project/detail?projectId="+projectRequest.getProjectId();
+        return "redirect:/project/" + projectRequest.getProjectId();
     }
 
 }
